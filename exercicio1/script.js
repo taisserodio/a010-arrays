8a19a0b399b26dba6eed696cb36b16638658770e
taisserodio <taisserodio12@gmail.com>
2022-09-01 19:07:10 -0300

    subi os exercicios de arrays10

---
+++ exercicio1/script.js
@@ -0,0 +1,17 @@
+//# Exercício 1
+
+//No primeiro exercício vamos praticar a criação de arrays. Crie 3 arrays diferentes: 
+//- O primeiro deve possuir apenas **números**, e estes números não devem seguir nenhuma ordem específica;
+const arraynumber = [1,5,9,7]
+
+
+//- O segundo array deve possuir apenas **strings**;
+const arrayString= ["arroz" , "feijao" , "carne"]
+
+//- O terceiro array deve possuir **números**, **strings** e **booleanos**.
+const arraymisto = ["boa tarde" , 1300 , false]
+
+//Imprima cada um dos arrays acima, utilizando `console.log()`
+console.log (arraynumber)
+console.log (arrayString)
+console.log (arraymisto)
